b31e4ef42a51e98e1c615a03219e6a988ad07f58
Patrick Sandquist <patricksandquist@gmail.com>
2015-09-18 15:16:53 -0700

    Add sub model

---
+++ app/models/sub.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id          :integer          not null, primary key
+#  title       :string           not null
+#  description :text
+#  user_id     :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Sub < ActiveRecord::Base
+  validates :title, :user_id, presence: true
+
+  belongs_to :moderator, class_name: "User"
+end
